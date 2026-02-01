ada61b01da636ecec3ba44462b5dec450069f492
Tanbir Alam <alamtanbir328@gmail.com>
2026-02-01 19:52:01 +0530

    fix: authentication refresh token issue

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,17 +1,17 @@
-import jwt from 'jsonwebtoken';
-import config from '../config/index.js';
+import jwt from "jsonwebtoken";
+import config from "../config/index.js";
 
 const verifyToken = (req, res, next) => {
-  const authHeader = req.headers['authorization'];
-  const token = authHeader && authHeader.split(' ')[1];
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
 
   if (!token) {
-    return res.status(401).json({ message: 'No token provided' });
+    return res.status(401).json({ message: "No token provided" });
   }
 
   jwt.verify(token, config.jwtSecret, (err, decoded) => {
     if (err) {
-      return res.status(403).json({ message: 'Invalid token' });
+      return res.status(401).json({ message: "Token expired or invalid" });
     }
     req.user = decoded;
     next();
