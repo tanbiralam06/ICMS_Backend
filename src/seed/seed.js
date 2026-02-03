754d4fb91c2124dfbbd564d5234d0acef29b118a
Tanbir Alam <alamtanbir328@gmail.com>
2026-02-03 20:50:59 +0530

    update the seed for creating Admin

--- src/seed/seed.js
+++ src/seed/seed.js
@@ -28,7 +28,7 @@ const seedData = async () => {
 
     // Create Admin User
     const adminUser = await User.create({
-      email: "admin@icms.com",
+      email: "tanbir@biomoneta.com",
       passwordHash: "admin123", 
       fullName: "System Admin",
       employeeId: "ADM001",
@@ -38,26 +38,26 @@ const seedData = async () => {
     console.log("Admin User Created:", adminUser.email);
 
     // Create Manager
-    const managerUser = await User.create({
-      email: "manager@icms.com",
-      passwordHash: "manager123",
-      fullName: "Tech Manager",
-      employeeId: "MGR001",
-      roleIds: ["Manager"],
-      departmentId: techDept._id,
-    });
-    console.log("Manager User Created:", managerUser.email);
+    // const managerUser = await User.create({
+    //   email: "manager@icms.com",
+    //   passwordHash: "manager123",
+    //   fullName: "Tech Manager",
+    //   employeeId: "MGR001",
+    //   roleIds: ["Manager"],
+    //   departmentId: techDept._id,
+    // });
+    // console.log("Manager User Created:", managerUser.email);
 
     // Create Employee
-    const employeeUser = await User.create({
-      email: "employee@icms.com",
-      passwordHash: "employee123",
-      fullName: "John Doe",
-      employeeId: "EMP001",
-      roleIds: ["Employee"],
-      departmentId: techDept._id,
-    });
-    console.log("Employee User Created:", employeeUser.email);
+    // const employeeUser = await User.create({
+    //   email: "employee@icms.com",
+    //   passwordHash: "employee123",
+    //   fullName: "John Doe",
+    //   employeeId: "EMP001",
+    //   roleIds: ["Employee"],
+    //   departmentId: techDept._id,
+    // });
+    // console.log("Employee User Created:", employeeUser.email);
 
     console.log("Seeding Completed Successfully.");
     process.exit(0);
